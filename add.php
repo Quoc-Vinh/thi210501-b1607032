c17ccae9d3d50b19d26d3d2453e62470441240a7
Quoc-Vinh <80906588+Quoc-Vinh@users.noreply.github.com>
2021-05-19 15:36:32 +0700

    Update add.php

--- add.php
+++ add.php
@@ -10,11 +10,13 @@
 		<form method="post">
 			 <p>So Xe</p><input type="text" name="soXe" value=""><br>
 			 <p>Nhan Hieu</p><input type="text" name="nhanhieu" value=""><br>
+			<p>id</p><input type="text" name="id" value=""><br>
 			 <input type="submit" name="submit" value="Submit">
 		</form>
 		<?php
 			$soXe=$_POST["soXe"];
 			$nhanhieu=$_POST["nhanhieu"];
+			$id=$_POST["id"];
 			function pg_connection_string_from_database_url(){
 				extract(parse_url($_ENV["DATABASE_URL"]));
 				return "user=$user password=$pass host=$host dbname=".substr($path,1);
@@ -25,12 +27,12 @@
 			}else{
 				echo "Opened database successfully\n";
 			}
-			$sql="INSERT INTO XE_OTO (soXe, nhanhieu) VALUES ('$soXe','$nhanhieu')";
+			$sql="INSERT INTO XE_OTO (id, soXe, nhanhieu) VALUES ('$id','$soXe','$nhanhieu')";
 			$ret = pg_query($db,$sql);
 			if(!$ret){
 				echo pg_last_error($db);
 			}else{
-				echo "Insert successfully\n"; Personid int NOT NULL AUTO_INCREMENT
+				echo "Insert successfully\n";
 				echo "$sql";
 			
 			}
